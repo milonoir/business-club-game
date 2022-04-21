d8e49b4c43402cbd79cf6126ebf75b55d5c7a97d
Milán Boleradszki <bmilan1985@gmail.com>
2022-04-21 23:02:15 +0100

    Drop "lobbyPlayer" type

--- lobby.go
+++ lobby.go
@@ -9,29 +9,23 @@ const (
 	maxPlayers = 4
 )
 
-type lobbyPlayer struct {
-	ready bool
-
-	*player
-}
-
 type lobby struct {
-	cnxs map[net.Conn]*lobbyPlayer
-	done chan struct{}
+	players map[net.Conn]*player
+	done    chan struct{}
 }
 
 func newLobby() *lobby {
 	return &lobby{
-		cnxs: make(map[net.Conn]*lobbyPlayer, maxPlayers),
-		done: make(chan struct{}),
+		players: make(map[net.Conn]*player, maxPlayers),
+		done:    make(chan struct{}),
 	}
 }
 
 func (l *lobby) joinPlayer(c net.Conn) {
 	log.Printf("player joined from: %s", c.RemoteAddr())
 
-	l.cnxs[c] = &lobbyPlayer{
-		player: &player{},
+	l.players[c] = &player{
+		conn: c,
 	}
 }
 
@@ -47,7 +41,7 @@ func (l *lobby) start() {
 
 func (l *lobby) stop() {
 	close(l.done)
-	for c := range l.cnxs {
+	for c := range l.players {
 		if err := c.Close(); err != nil {
 			log.Printf("error closing connection %s: %s", c.RemoteAddr(), err)
 		}
--- player.go
+++ player.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"net"
+)
+
 type player struct {
+	// Networking properties
+	conn  net.Conn
+	ready bool
+
+	// Game properties
 	cash    int
 	stocks  map[string]int
 	actions []card
