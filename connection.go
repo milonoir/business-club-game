2aa06b0770e4e048ac67d66eb48408e8f9c2f68f
Milán Boleradszki <bmilan1985@gmail.com>
2023-05-12 17:32:30 +0100

    progress with connection

--- connection.go
+++ connection.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net"
+	"sync/atomic"
+	"time"
 
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 	"github.com/milonoir/bc-server/message"
 )
 
+const (
+	pingInterval = 2 * time.Second
+)
+
 type connection struct {
 	conn net.Conn
 	done chan struct{}
 
-	// channels for communicating with the server/lobby.
-	in  <-chan message.Message
-	out chan<- message.Message
+	lastPong time.Time
+	drop     atomic.Bool
+	incoming chan message.Message
 }
 
-func newConnection() *connection {
-	return &connection{}
+func newConnection(conn net.Conn) *connection {
+	c := &connection{
+		conn:     conn,
+		done:     make(chan struct{}),
+		incoming: make(chan message.Message),
+	}
+
+	go c.ping()
+	go c.receive()
+
+	return c
 }
 
 func (c *connection) ping() {
+	t := time.NewTicker(pingInterval)
+	defer t.Stop()
+
 	for {
 		select {
 		case <-c.done:
 			return
-		default:
+		case <-t.C:
 			if _, err := c.conn.Write(ws.CompiledPing); err != nil {
 				log.Printf("ERROR - sending ping to client (%s): %v", c.conn.RemoteAddr(), err)
 			}
@@ -35,21 +54,56 @@ func (c *connection) ping() {
 	}
 }
 
-func (c *connection) reader() {
+func (c *connection) receive() {
 	for {
+		select {
+		case <-c.done:
+			return
+		default:
+		}
+
 		data, op, err := wsutil.ReadClientData(c.conn)
 		if err != nil {
 			log.Printf("ERROR - read client (%s) data: %v", c.conn.RemoteAddr(), err)
 		}
 
-		_ = data
-
 		switch op {
 		case ws.OpPong:
-		case ws.OpClose:
+			c.lastPong = time.Now()
 		case ws.OpText:
+			if c.drop.Load() {
+				continue
+			}
+			m, err := message.Parse(data)
+			if err != nil {
+				log.Printf("ERROR - parse message (%s): %v", c.conn.RemoteAddr(), err)
+				continue
+			}
+			select {
+			case c.incoming <- m:
+			default:
+			}
 		default:
-
 		}
 	}
+}
+
+func (c *connection) send(msg message.Message) {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		log.Printf("ERROR - corrupt message: %v", err)
+		return
+	}
+
+	if err = wsutil.WriteClientMessage(c.conn, ws.OpText, b); err != nil {
+		log.Printf("ERROR - write client (%s) message: %v", c.conn.RemoteAddr(), err)
+	}
+}
+
+func (c *connection) dropMessages() {
+	c.drop.Store(true)
+}
+
+func (c *connection) keepMessages() {
+	c.drop.Store(false)
 }
